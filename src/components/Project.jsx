2ecdafd9cdba49600f37e7e19f5189e2a1477f8a
kurlee710 <josephatamaniuk@gmail.com>
2024-10-08 15:40:38 -0400

    styled the img div in project.jsx to have a max width of 450px and a max height of 300px

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,11 @@ const Project = ({ title, description, image, liveLink, repoLink }) => {
   return (
     <div className="project-item">
       <h3>{title}</h3>
-      <img src={image} alt={`${title} screentshot`} />
+      <img
+        src={image}
+        alt={`${title} screentshot`}
+        style={{ width: "450pxx", height: "300px", objectFit: "cover" }}
+      />
       <p>{description}</p>
       <div classname="project-links">
         <a href={liveLink} target="_blank" rel="noreferrer">
