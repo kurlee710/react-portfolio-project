168fba88a782d7101bcf731508953765349677bd
kurlee710 <josephatamaniuk@gmail.com>
2024-10-08 16:18:32 -0400

    added bucket list project with image link and description

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Project from "../components/Project";
 import quizImage from "../assets/images/quiz-me-screenshot.png"; // Import the image
-
+import bucketListImage from "../assets/images/bucketlist-screenshot.png"; // Import the image
 const Portfolio = () => {
   const projects = [
     {
@@ -11,7 +11,13 @@ const Portfolio = () => {
       repoLink: "https://github.com/kurlee710/quiz-me-repo",
       description: "Description of project 1.",
     },
-    { title: "Project 2", description: "Description of project 2." },
+    {
+      title: "Project 2",
+      image: bucketListImage,
+      liveLink: "https://heartfelt-raindrop-1cfaab.netlify.app",
+      repoLink: "https://github.com/kurlee710/bucketlist-app",
+      description: "Description of project 2.",
+    },
     { title: "Project 3", description: "Description of project 3." },
   ];
 
