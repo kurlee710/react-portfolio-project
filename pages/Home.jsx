3793665273a7d1b78b8361211828b7878ee6460c
kurlee710 <josephatamaniuk@gmail.com>
2024-10-07 22:46:52 -0400

    added boilerplate code to home.jsx

---
+++ pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to My Portfolio</h2>
+      <p>Explore my work and projects!</p>
+    </div>
+  );
+};
+
+export default Home;
