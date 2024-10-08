bfd11b9fffefafd5f2a9d5a495e9f36c828b15e2
kurlee710 <josephatamaniuk@gmail.com>
2024-10-07 22:50:48 -0400

    added boilerplate code to contact.jsx

---
+++ pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>Email: your-email@example.com</p>
+      <p>Phone: (123) 456-7890</p>
+    </div>
+  );
+};
+
+export default Contact;
