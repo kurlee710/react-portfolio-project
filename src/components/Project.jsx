e5f8babd0fe7afa1f0c2d8396f4c577e8396665c
kurlee710 <josephatamaniuk@gmail.com>
2024-10-08 15:02:07 -0400

    added img div to project.jsx with alt tag

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Project = ({ title, description }) => {
+const Project = ({ title, description, image, liveLink, repoLink }) => {
   return (
     <div className="project-item">
       <h3>{title}</h3>
+      <img src={image} alt={`${title} screentshot`} />
       <p>{description}</p>
     </div>
   );
