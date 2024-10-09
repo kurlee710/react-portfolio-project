a363b0de5b3fb25fcbe06a92c8a1198991287fef
kurlee710 <josephatamaniuk@gmail.com>
2024-10-09 15:30:44 -0400

    added font awesome script to index.html. Added github logo to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
+      <i class="fab fa-github" style={{ fontSize: "40px" }}></i>
       <p>&copy; 2024 My Portfolio</p>
     </footer>
   );
